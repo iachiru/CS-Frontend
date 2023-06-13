1fcdfaafde3d52447832d816d470a22e2c70ab06
iachiru <108947252+iachiru@users.noreply.github.com>
2023-06-13 18:27:20 +0100

    styled kitchens on homepage and styled some of the jumbotron

--- src/components/AllKitchens.jsx
+++ src/components/AllKitchens.jsx
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import { useState } from "react";
 import KitchenImgCarousel from "./KitchenImgCarousel";
 import Axios from "axios";
+import { Button, Col } from "react-bootstrap";
 
 function AllKitchens() {
   const [data, setData] = useState("");
@@ -22,16 +23,17 @@ function AllKitchens() {
     data && (
       <>
         {data.map((kitchen, i) => (
-          <div key={i}>
-            <KitchenImgCarousel data={kitchen.images} />
-            <h5>Kitchen {kitchen.ref}</h5>
-            <h6>
-              Type:
-              {kitchen.kitchenType}{" "}
-            </h6>
-            <h5> About </h5>
-            <p>{kitchen.description}</p>
-            <h5>Address:</h5> {kitchen.address} <h5>Price:</h5> {kitchen.price}
+          <div className="kitchen-container">
+            <Col key={i}>
+              <KitchenImgCarousel data={kitchen.images} />
+            </Col>
+            <Col className="d-flex flex-column justify-content-around align-items-center">
+              <h4>Kitchen {kitchen.ref}</h4>
+              <h6>Type: {kitchen.kitchenType}</h6>
+              <h6>Address: {kitchen.address}</h6>
+              <h6>Price: {kitchen.price}</h6>
+              <Button className="kitchen-card-button">More details</Button>
+            </Col>
           </div>
         ))}
       </>
